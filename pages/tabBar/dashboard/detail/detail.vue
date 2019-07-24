ac1fd52170464a95519ec3257b0fbf9eb3a3e639
Galaxy_G <269274122@qq.com>
2019-07-24 16:03:18 +0800

    登录页跳转

--- pages/tabBar/dashboard/detail/detail.vue
+++ pages/tabBar/dashboard/detail/detail.vue
@@ -1,44 +1,44 @@
 <template>
-	<view class="example">
-		<view class="example-title">纵向排列</view>
-		<uni-steps :options="list2" :active="active" direction="column" />
-	</view>
+<view class="example">
+    <view class="example-title">纵向排列</view>
+    <uni-steps :options="list2" :active="active" direction="column" />
+</view>
 </template>
 <script>
-	import uniSteps from '@/components/uni-steps/uni-steps.vue'
-	export default {
-		components: {
-			uniSteps
-		},
-		data() {
-			return {
-				title: '',
-					active: 1,
-				list2: [{
-					title: '买家下单',
-					desc: '2018-11-11'
-				}, {
-					title: '卖家发货',
-					desc: '2018-11-12'
-				}, {
-					title: '买家签收',
-					desc: '2018-11-13'
-				}, {
-					title: '交易完成',
-					desc: '2018-11-14'
-				}]
-			}
-		},
-		onLoad(e) {
-			this.title = e.title || '';
-		}
-	}
+import uniSteps from '@/components/uni-steps/uni-steps.vue'
+export default {
+    components: {
+        uniSteps
+    },
+    data() {
+        return {
+            title: '',
+            active: 1,
+            list2: [{
+                title: '买家下单',
+                desc: '2018-11-11'
+            }, {
+                title: '卖家发货',
+                desc: '2018-11-12'
+            }, {
+                title: '买家签收',
+                desc: '2018-11-13'
+            }, {
+                title: '交易完成',
+                desc: '2018-11-14'
+            }]
+        }
+    },
+    onLoad(e) {
+        this.title = e.title || '';
+    }
+}
 </script>
 <style>
-	page {
-		display: flex;
-		flex-direction: column;
-		box-sizing: border-box;
-		background-color: #62c2e7
-	}
+page {
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    background-color: #62c2e7
+}
 </style>
